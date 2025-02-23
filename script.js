19ce582deff1ca9c751943def4d152431140b3c0
Maurice Jah <mauricejahwizwid@gmail.com>
2025-02-23 23:43:54 +0100

    Implement Loan

--- script.js
+++ script.js
@@ -173,6 +173,19 @@ btnTransfer.addEventListener('click', function (e) {
     updateUI(currentAccount);
   }
 });
+
+//  IMPLEMENTING LOAN
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov > amount * 0.1)) {
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+  }
+
+  inputLoanAmount.value = '';
+});
+
 // CLOSE ACCOUNT --- USING FINDINDEX
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
